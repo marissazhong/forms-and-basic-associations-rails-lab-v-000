ff2dca0c3accfd87ffc38c1a596179ae6ff2d8b0
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 15:03:30 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,8 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents=(notes)
     notes.each do |content|
       if content.strip != ""
-        note = Note.find_or_create_by(content: content)
-        self.notes << note
+        self.notes.build(content: content)
       end
     end
   end
