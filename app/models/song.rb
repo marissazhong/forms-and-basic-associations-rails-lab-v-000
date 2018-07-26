e215dd2e32cee9760a9220db69f5902de6ad125b
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 14:31:42 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,6 +6,7 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+  
   def artist_name
     self.artist ? self.artist.name : nil
   end
