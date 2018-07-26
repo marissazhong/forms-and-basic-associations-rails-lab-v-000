19312157b47bc97bbad374a6578658729fe16d72
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 14:42:11 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -18,4 +18,10 @@ class Song < ActiveRecord::Base
   def genre_name
     self.genre ? self.genre.name : nil
   end
+
+  def note_contents=(notes)
+    notes.each do |id|
+      note = Note.find(id)
+      self.notes << note
+    end
 end
