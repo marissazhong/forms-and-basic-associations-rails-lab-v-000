477f937ba56808bcaf9a5b412ff697b383a8696f
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 15:09:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create(song_params)
+    @song = Song.new(song_params)
 
     if @song.save
       redirect_to @song
