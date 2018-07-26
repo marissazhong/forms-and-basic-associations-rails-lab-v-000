556a2ec43475a3b73a1febb743e105d65aab423d
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 15:06:39 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,25 +25,6 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
-  def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
-
   private
 
   def song_params
